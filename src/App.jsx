4305151d56ed6ab3cdde1c10fcbacd72e75e5134
Ljusaj123 <rosana.ljusaj2@gmail.com>
2024-01-02 16:58:16 +0100

    set up the project, implemented router, addded npm packages, added tailwind and daisyUI

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from "./pages";
+
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "products/:id",
+        element: <SingleProduct />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
+
+function App() {
+  return (
+    <RouterProvider router={router}>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </RouterProvider>
+  );
+}
+
+export default App;
