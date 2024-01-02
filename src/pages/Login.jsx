4bf8a30165f5629ba5fb04153e1213ec6963bcad
Ljusaj123 <rosana.ljusaj2@gmail.com>
2024-01-02 17:41:43 +0100

    created input and button components for input and included in Login, created structure for Login page, chenged rule for eslint

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,44 @@
+import { FormInput, SubmitBtn } from "../components";
+import { Form, Link } from "react-router-dom";
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <section className="h-screen grid place-items-center">
+      <Form
+        method="post"
+        className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4'"
+      >
+        <h4 className="text-center text-3xl font-bold">Login</h4>
+        <FormInput
+          name="identifier"
+          type="email"
+          defaultValue="test@test.com"
+          label="email"
+        />
+        <FormInput
+          name="password"
+          type="password"
+          defaultValue="secret"
+          label="password"
+        />
+        <div className="mt-4">
+          <SubmitBtn text="login" />
+        </div>
+        <button type="button" className="btn btn-secondary btn-block mt-4">
+          guest user
+        </button>
+        <p className="text-center mt-4">
+          Not a member yet?
+          <Link
+            to="/register"
+            className="ml-2 link link-hover link-primary capitalize"
+          >
+            register
+          </Link>
+        </p>
+      </Form>
+    </section>
+  );
 }
 
 export default Login;
