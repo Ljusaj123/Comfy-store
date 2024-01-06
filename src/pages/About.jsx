cb6ee820233f93103ec53c1112b38586572ec6b6
Ljusaj123 <rosana.ljusaj2@gmail.com>
2024-01-06 15:45:00 +0100

    created About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,25 @@
 function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight">
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, cumque?
+        Corporis, numquam vel molestiae similique ad odio quae ea quis quas eos,
+        ab nobis id inventore magni dolore quibusdam beatae.
+      </p>
+    </>
+  );
 }
 
 export default About;
