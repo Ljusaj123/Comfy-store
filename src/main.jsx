d6cf13f23b48b8818a8a5c4d5c674e4e6c715b1b
Ljusaj123 <rosana.ljusaj2@gmail.com>
2024-01-19 22:34:31 +0100

    setup redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { ToastContainer } from "react-toastify";
+import { store } from "./store";
+import { Provider } from "react-redux";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+      <ToastContainer position="top-center" />
+    </React.StrictMode>
+  </Provider>
+);
